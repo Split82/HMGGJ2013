fc0750874c77cf71f9e28b27130de6f14e3012c5
Lukas Foldyna <lukas.foldyna@arat.cz>
2013-01-25 20:24:20 +0100

    Added player model

---
+++ HMGGJ2013/SRC/PlayerModel.h
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.h
+//  HMGGJ2013
+//
+//  Created by Lukáš Foldýna on 25.01.13.
+//  Copyright (c) 2013 Hyperbolic Magnetism. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlayerModel : NSObject
+
+@end
