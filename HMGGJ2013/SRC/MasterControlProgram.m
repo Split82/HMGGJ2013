8cc44162ad6e5ae6146991b189e69b7865d0f836
Peter Morihladko <morihladko@gmail.com>
2013-01-26 19:26:58 +0100

    They come in waves

--- HMGGJ2013/SRC/MasterControlProgram.m
+++ HMGGJ2013/SRC/MasterControlProgram.m
@@ -13,24 +13,26 @@
 
 // init values
 const float INIT_ENEMIES_PER_WAVE = 4.0f;
-const float INIT_WAVE_PERIOD = 10.0f; // seconds
+const float INIT_WAVE_PERIOD = 15.0f; // seconds
 
 const float INIT_SWIPE_TAP_ENEMIES_RATIO = 0.2;
 
 // constant values
-const float ENEMIES_GROWTH_PER_WAVE = 0.7;
+const float ENEMIES_GROWTH_PER_WAVE = 0.9;
 const float SWIPE_TAP_RATIO_GROWTH_PER_WAVE = 0.05;
-const float WAVE_LENGHT_DECREASE = 0.2; // second
+const float WAVE_LENGHT_DECREASE = 0.75; // second
 
-const float MIN_WAVE_LENGTH = 4.0f; // seconds
+const float MIN_WAVE_LENGTH = 5.0f; // seconds
 const float MAX_ENEMIES_PER_WAVE = 30.0f;
-const float MAX_TAP_RATION_PER_WAVE = 0.5;
+const float MAX_TAP_RATION_PER_WAVE = 0.6;
 
-const float ENEMY_SPAWN_TIME = 1.0f;
-const float ENEMY_SPAWN_DELTA_TIME = 2.0f;
+const float ENEMY_SPAWN_TIME = 2.0f;
+const float ENEMY_SPAWN_DELTA_TIME = 4.0f;
 
 const float WAVE_ENEMY_SPAWN_TIME = 0.5f;
-const float WAVE_ENEMY_SPAWN_DELTA_TIME = 1.0f;
+const float WAVE_ENEMY_SPAWN_DELTA_TIME = 0.5f;
+
+const float INCREASE_SPAWN_SPEED_FACTOR = 1.25f;
 
 float increase(float value, float inc, float MAX) {
     if (value < MAX) {
@@ -65,33 +67,40 @@ float decrease(float value, float dec, float MIN) {
     
     // per wave
     int enemiesToGenerate;
+    float waveSpawnSpeedFactor;
 }
 
 - (id)init {
     self = [super init];
     
     if (self) {
-        [self scheduleNewEnemySpawn];
-        
         enemiesPerWave = INIT_ENEMIES_PER_WAVE - ENEMIES_GROWTH_PER_WAVE;
         wavePeriod = INIT_WAVE_PERIOD + WAVE_LENGHT_DECREASE;
         swipeTapRatio = INIT_SWIPE_TAP_ENEMIES_RATIO - SWIPE_TAP_RATIO_GROWTH_PER_WAVE;
+        
+        enemySpawnTime = ENEMY_SPAWN_TIME;
+        enemySpawnTimeDelta = ENEMY_SPAWN_DELTA_TIME;
+        
+        waveSpawnSpeedFactor = 1.0f;
+        
+        [self scheduleNewEnemySpawn];
     }
     
     return self;
 }
 
 - (void)calc:(ccTime)deltaTime; {
-    nextWaveTime -= nextWaveTime;
+    nextWaveTime -= deltaTime;
     nextEnemySpawnTime -= deltaTime;
 
-    if (nextEnemySpawnTime < 0) {
-        [self spawnEnemy];
-    }
-    
     if (nextWaveTime < 0) {
         [self startWave];
     }
+    
+    if (nextEnemySpawnTime < 0) {
+        [self spawnEnemy];
+    }
+
 }
 
 
@@ -102,25 +111,34 @@ float decrease(float value, float dec, float MIN) {
 
 
 - (void)startWave {
+    // previous wave is not finished
+    if (enemiesToGenerate > 0) {
+        waveSpawnSpeedFactor = waveSpawnSpeedFactor * INCREASE_SPAWN_SPEED_FACTOR;
+    }
+    
     enemiesPerWave = increase(enemiesPerWave, ENEMIES_GROWTH_PER_WAVE, MAX_ENEMIES_PER_WAVE);
     swipeTapRatio = increase(swipeTapRatio, SWIPE_TAP_RATIO_GROWTH_PER_WAVE, MAX_TAP_RATION_PER_WAVE);
     wavePeriod = decrease(wavePeriod, WAVE_LENGHT_DECREASE, MIN_WAVE_LENGTH);
     
     enemiesToGenerate = (int)round(enemiesPerWave);
+    NSLog(@"Starting wave, spawning %d enemies", enemiesToGenerate);
     
-    enemySpawnTime = WAVE_ENEMY_SPAWN_TIME;
-    enemySpawnTimeDelta = WAVE_ENEMY_SPAWN_DELTA_TIME;
+    enemySpawnTime = WAVE_ENEMY_SPAWN_TIME / waveSpawnSpeedFactor;
+    enemySpawnTimeDelta = WAVE_ENEMY_SPAWN_DELTA_TIME / waveSpawnSpeedFactor;
     
     nextWaveTime = wavePeriod;
+    nextEnemySpawnTime = 0;
 }
 
 - (void)spawnEnemy {
     
     // TODO for now, it's just randomness, not calculating true ration of spawned enemis
     if ((float)arc4random() / ARC4RANDOM_MAX < swipeTapRatio) {
+        NSLog(@"Enemy Swipe");
         [self.mainframe addEnemy:kEnemyTypeSwipe];
     
     } else {
+        NSLog(@"Enemy Tap");
         [self.mainframe addEnemy:kEnemyTypeTap];
     }
     
@@ -130,6 +148,7 @@ float decrease(float value, float dec, float MIN) {
         
         // end of wave spawning
         if (enemiesToGenerate == 0) {
+            NSLog(@"Enging wave\n");
             enemySpawnTime = ENEMY_SPAWN_TIME;
             enemySpawnTimeDelta = ENEMY_SPAWN_DELTA_TIME;
         }
