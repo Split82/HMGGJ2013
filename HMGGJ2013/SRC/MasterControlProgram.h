96d437984e402d235f50474af1d89c04b76b527b
Peter Morihladko <morihladko@gmail.com>
2013-01-26 15:33:15 +0100

    MasterControlProgram

---
+++ HMGGJ2013/SRC/MasterControlProgram.h
@@ -0,0 +1,13 @@
+//
+//  MasterControlProgram.h
+//  HMGGJ2013
+//
+//  Created by Peter Morihladko on 1/26/13.
+//  Copyright (c) 2013 Hyperbolic Magnetism. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MasterControlProgram : NSObject
+
+@end
