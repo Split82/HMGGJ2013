177ab2a88bea139113e6d19fb2f1a70e2cac02ba
Peter Morihladko <morihladko@gmail.com>
2013-01-26 15:45:54 +0100

    FIX Sound Effect looping

--- HMGGJ2013/SRC/AudioManager.m
+++ HMGGJ2013/SRC/AudioManager.m
@@ -51,7 +51,7 @@ const int BUFF_EFFECTS = kASC_Right;
 
 - (void)playEffect:(int)soundId {
     
-    [soundEngine playSound:soundId sourceGroupId:BUFF_EFFECTS pitch:1.0f pan:0.0f gain:1.0f loop:YES];
+    [soundEngine playSound:soundId sourceGroupId:BUFF_EFFECTS pitch:1.0f pan:0.0f gain:1.0f loop:NO];
 }
 
 - (void)preloadSounds {
