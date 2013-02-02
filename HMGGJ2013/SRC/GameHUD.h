f54da8d275b3ce6be3fe3263e9a51d85c75c84f4
Jan Ilavsky <hyperbolicmagnetism@gmail.com>
2013-02-02 18:04:46 +0100

    GameHUD

--- HMGGJ2013/SRC/GameHUD.h
+++ HMGGJ2013/SRC/GameHUD.h
@@ -1,32 +1,13 @@
 //
-//  GameHUD.h
+//  HUD.h
 //  HMGGJ2013
 //
 //  Created by Jan Ilavsky on 2/2/13.
 //  Copyright (c) 2013 Hyperbolic Magnetism. All rights reserved.
 //
 
-@protocol GameHUDDelegate;
+#import <Foundation/Foundation.h>
 
-
-@interface GameHUD : NSObject
-
-@property (nonatomic, weak) id <GameHUDDelegate> delegate;
-@property (nonatomic, assign) int score;
-@property (nonatomic, assign) int numberOfCoins;
-@property (nonatomic, assign) float rageProgress; // 0 - 1
-@property (nonatomic, readonly) CGPoint coinsSpritePosition;
-
-- (id)initWithParentView:(UIView*)parentView parentNode:(CCNode*)parentNode parentSpriteBatchNode:(CCNode*)parentSpriteBatchNode bounds:(CGRect)bounds;
-
-- (void)show;
-- (void)hide;
-
-@end
-
-
-@protocol GameHUDDelegate <NSObject>
-
-- (void)gameHUDPauseButtonWasPressed:(GameHUD*)gameHUD;
+@interface HUD : NSObject
 
 @end
